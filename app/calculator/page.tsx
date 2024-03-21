69075842985d04fe391db40334fa79d742d2b02e
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-21 14:59:10 +0600

    updated calculator design

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -46,15 +46,16 @@ export default function Home() {
       setInput(input + value);
     }
   };
+
   return (
     <div
       className="py-12 flex justify-center  items-center p-3"
       onKeyDown={handleKeyDown}
     >
-      <div className="shadow-2xl max-w-md bg-white rounded-md p-4">
-        <div className="border text-xl mb-2 text-white rounded-md overflow-hidden font-semibold">
+      <div className="shadow max-w-[320px] bg-gray-600 rounded-2xl p-4">
+        <div className="border border-gray-500 text-xl mb-5 text-gray-100 rounded-md overflow-hidden font-semibold">
           <input
-            className="p-2 bg-blue-950  w-full focus:outline-none"
+            className="p-2 bg-gray-900  w-full focus:outline-none"
             type="text"
             name="input"
             value={input}
@@ -62,7 +63,7 @@ export default function Home() {
             placeholder="0"
           />
           <input
-            className="p-2 bg-blue-950 w-full focus:outline-none"
+            className="p-2 bg-gray-900 w-full focus:outline-none"
             type="text"
             name="result"
             value={result}
@@ -70,112 +71,112 @@ export default function Home() {
             readOnly
           />
         </div>
-        <div className="grid gap-4 grid-cols-4">
+        <div className="grid gap-4 text-gray-100 grid-cols-4">
           <button
             type="button"
-            className="shadow-md bg-gray-100 py-4 px-6"
+            className="shadow flex justify-center bg-gray-800 rounded-xl py-4 px-6"
             onClick={handleAllClear}
           >
             AC
           </button>
           <button
             type="button"
-            className="shadow-md bg-gray-100 py-4 px-6"
+            className="shadow bg-gray-800 rounded-xl py-4 px-6"
             onClick={handleClear}
           >
             C
           </button>
           <button
             type="button"
-            className="shadow-md bg-gray-100 py-4 px-6"
+            className="shadow bg-gray-800 rounded-xl py-4 px-6"
             onClick={() => handleClick("%")}
           >
             %
           </button>
           <button
             type="button"
-            className="shadow-md bg-gray-100 py-4 px-6"
+            className="shadow bg-gray-800 rounded-xl py-4 px-6"
             onClick={() => handleClick("/")}
           >
             /
           </button>
         </div>
-        <div className="grid grid-cols-4 gap-4 mt-4">
+        <div className="grid grid-cols-4 text-gray-100 gap-4 mt-4">
           <div className="col-span-3">
-            <div className="grid grid-cols-3 ">
+            <div className="grid grid-cols-3 gap-4 ">
               <button
                 type="button"
-                className="py-4 shadow-md bg-gray-200 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("7")}
               >
                 7
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("8")}
               >
                 8
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("9")}
               >
                 9
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("4")}
               >
                 4
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("5")}
               >
                 5
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("6")}
               >
                 6
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("1")}
               >
                 1
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("2")}
               >
                 2
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className="py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick("3")}
               >
                 3
               </button>
               <button
                 type="button"
-                className=" py-4 px-6 col-span-2"
+                className=" py-4 px-6 shadow  bg-gray-800 rounded-xl col-span-2"
                 onClick={() => handleClick("0")}
               >
                 0
               </button>
               <button
                 type="button"
-                className=" py-4 px-6"
+                className=" py-4 shadow  bg-gray-800 rounded-xl px-6"
                 onClick={() => handleClick(".")}
               >
                 .
@@ -183,31 +184,31 @@ export default function Home() {
             </div>
           </div>
           {/* functions */}
-          <div className="grid bg-green-50 gri-cols-1">
+          <div className="grid  gri-cols-1 gap-4">
             <button
               type="button"
-              className=" py-4 px-6"
+              className=" py-4 shadow  bg-gray-800 rounded-xl px-6"
               onClick={() => handleClick("*")}
             >
               x
             </button>
             <button
               type="button"
-              className=" py-4 px-6"
+              className=" py-4 shadow  bg-gray-800 rounded-xl px-6"
               onClick={() => handleClick("-")}
             >
               -
             </button>
             <button
               type="button"
-              className=" py-4 px-6"
+              className=" py-4 shadow  bg-gray-800 rounded-xl px-6"
               onClick={() => handleClick("+")}
             >
               +
             </button>
             <button
               type="button"
-              className=" py-4 px-6"
+              className=" py-4 shadow  bg-gray-800 rounded-xl px-6"
               onClick={() => handleClick("=")}
             >
               =
